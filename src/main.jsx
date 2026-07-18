1a2f3d4af1a0179e917ed2114b34373f284296fe
Prathamesh Mowade <prathameshmowade@gmail.com>
2026-07-18 14:20:05 +0530

    Setup React root

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EMGDashboard from "./EMGDashboard_v5.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EMGDashboard />
+  </React.StrictMode>
+);
